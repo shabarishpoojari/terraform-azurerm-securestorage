477b89a8fb1e7029e4a86e3576fa4c2458d8774c
shabarishpoojari <shabarishpoojari1992@gmail.com>
2026-01-08 04:50:59 +0000

    added main.tf variable.tf and outputs.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "environment" {
+  type        = string
+  description = "Env Variable"
+}
+
+variable "resource_group_name" {
+  type        = string
+  description = "Resource Group Name"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "storage account name"
+}
+
+variable "location" {
+  type        = string
+  description = "location name"
+}
